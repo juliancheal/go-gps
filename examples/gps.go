d690c35bedecf71fbafd8d462f4ee13ca12a68af
Julian Cheal <julian.cheal@gmail.com>
2014-07-24 06:38:42 +0300

    Major refactor, moved code into it's own package. Buit and example of how to call the gps in the Example folder.

---
+++ examples/gps.go
@@ -0,0 +1,19 @@
+package main
+ 
+import (
+  "fmt"
+  // "github.com/juliancheal/go-gps"
+  "../gps"
+)
+
+func main() {
+  for {
+    gga, _ := gps.ParseNmea("/dev/tty.SLAB_USBtoUART", 4800)
+    lat := gga.Latitude
+    lon := gga.Longitude
+    if lat != "" {
+      fmt.Println(lat)
+      fmt.Println(lon)
+    }
+  }
+}
--- gps/nmea.go
+++ gps/nmea.go
@@ -24,6 +24,8 @@ type Nmea struct {
 	Dgps            string
 }
 
+var _nmea = []string{}
+
 func latlngToDecimal(coord string, dir string, lat bool) string {
 	decimal := 0.0
 	negative := false
@@ -49,37 +51,42 @@ func latlngToDecimal(coord string, dir string, lat bool) string {
 	return _decimal
 }
 
+func parser(raw string) (nmea Nmea, err error) {
+  err = nil
+  line := strings.Split(raw, ",")
+  t := strings.Split(line[0], "")
+ 
+  if 0 < len(t) {
+    temp := t[2:5]
+    switch strings.Join(temp, "") {
+    case "GGA":
+      gga := Nmea{
+        LastNmea:        line[0],
+        Time:            line[1],
+        Latitude:        line[2],
+        LatRef:          line[3],
+        Longitude:       line[4],
+        LongRef:         line[5],
+        Quality:         line[6],
+        NumSat:          line[7],
+        Hdop:            line[8],
+        Altitude:        line[9],
+        AltUnit:         line[10],
+        HeightGeoid:     line[11],
+        HeightGeoidUnit: line[12],
+        LastDgps:        line[13],
+        Dgps:            line[14],
+      }
+      gga.Latitude = latlngToDecimal(gga.Latitude, gga.LatRef, true)
+      gga.Longitude = latlngToDecimal(gga.Longitude, gga.LongRef, true)
+      return gga, err
+    }
+  }
+  return Nmea{}, err
+}
+
 // $GPGGA,221440.069,3033.2807,N,08126.6636,W,1,05,1.7,-20.0,M,-31.7,M,,0000*72
-func ParseNMEA(raw string) Nmea {
-
-	line := strings.Split(raw, ",")
-	t := strings.Split(line[0], "")
-
-	if 0 < len(t) {
-		temp := t[2:5]
-		switch strings.Join(temp, "") {
-		case "GGA":
-			gga := Nmea{
-				LastNmea:        line[0],
-				Time:            line[1],
-				Latitude:        line[2],
-				LatRef:          line[3],
-				Longitude:       line[4],
-				LongRef:         line[5],
-				Quality:         line[6],
-				NumSat:          line[7],
-				Hdop:            line[8],
-				Altitude:        line[9],
-				AltUnit:         line[10],
-				HeightGeoid:     line[11],
-				HeightGeoidUnit: line[12],
-				LastDgps:        line[13],
-				Dgps:            line[14],
-			}
-			gga.Latitude = latlngToDecimal(gga.Latitude, gga.LatRef, true)
-			gga.Longitude = latlngToDecimal(gga.Longitude, gga.LongRef, true)
-			return gga
-		}
-	}
-	return Nmea{}
+func ParseNMEA(raw string) (nmea Nmea, err error) {
+  gga, err := parser(raw)
+  return gga, err
 }
